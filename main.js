1e3c98591b83d37d55a38cc279ee99469bc08ac9
abdulkadir <75859682+abdulkadirpolat@users.noreply.github.com>
2021-01-31 19:44:12 +0300

    Initial commit

--- main.js
+++ main.js
@@ -2,24 +2,90 @@
 
 function eventListener (){
 window.addEventListener('keyup',sounds)
+window.addEventListener('click',keyTouch) 
+}
+function sounds(e){
+  
+   const sound = document.querySelector(  `audio[data-key='${e.keyCode}']`) ;
+   const keys = document.querySelector(  `.key[data-key='${e.keyCode}']`) ;
+if(!sound){
+   return ;
+} 
+
+  else{
+   sound.currentTime = 0;
+   sound.play(); 
+  keys.classList.add('play') 
+
+  setTimeout( ()=> keys.classList.remove('play'),100)   
+  } 
+ }
+
+function keyTouch(e){
+ const val =Number( e.target.value)
+ const sound = document.querySelector(  `audio[value='${e.target.value}']`) ;
+ const keys = document.querySelector(  `.key[value='${e.target.value}']`) ;
  
+switch(val ){
+   case 65:
+      sound.currentTime = 0;
+      sound.play(); 
+     keys.classList.add('play') 
+      
+     setTimeout( ()=> keys.classList.remove('play'),100) 
+   break;
+   case 83:
+      sound.currentTime = 0;
+      sound.play(); 
+     keys.classList.add('play') 
+     setTimeout( ()=> keys.classList.remove('play'),100) 
+   break;
+   case 68:
+      sound.currentTime = 0;
+      sound.play(); 
+     keys.classList.add('play') 
+     setTimeout( ()=> keys.classList.remove('play'),100) 
+   break;
+   case 70:
+      sound.currentTime = 0;
+      sound.play(); 
+     keys.classList.add('play') 
+     setTimeout( ()=> keys.classList.remove('play'),100) 
+   break;
+   case 71:
+      sound.currentTime = 0;
+      sound.play(); 
+     keys.classList.add('play') 
+     setTimeout( ()=> keys.classList.remove('play'),100) 
+   break;
+   case 72:
+      sound.currentTime = 0;
+      sound.play(); 
+     keys.classList.add('play') 
+     setTimeout( ()=> keys.classList.remove('play'),100) 
+   break;
+   case 74:
+      sound.currentTime = 0;
+      sound.play(); 
+     keys.classList.add('play') 
+     setTimeout( ()=> keys.classList.remove('play'),100) 
+   break;
+   case 75:
+      sound.currentTime = 0;
+      sound.play(); 
+     keys.classList.add('play') 
+     setTimeout( ()=> keys.classList.remove('play'),100) 
+   break;
+   case 76:
+      sound.currentTime = 0;
+      sound.play(); 
+     keys.classList.add('play') 
+     setTimeout( ()=> keys.classList.remove('play'),100) 
+   break;
 }
 
-function sounds(e){
- 
-    const sound = document.querySelector(  `audio[data-key='${e.keyCode}']`) ;
-    const keys = document.querySelector(  `.key[data-key='${e.keyCode}']`) ;
- if(!sound){
-    return ;
- } 
 
-   else{
-    sound.currentTime = 0;
-    sound.play(); 
-   keys.classList.add('play') 
 
-   setTimeout( ()=> keys.classList.remove('play'),100)   
-   } 
-  }
+}
 
  
